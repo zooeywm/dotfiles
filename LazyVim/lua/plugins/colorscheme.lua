5b9525934a6421c1ac0d375f563b063b02a76d7c
zooeywm <zooeywm@gmail.com>
2024-08-27 18:24:42 +0800

    2024-08-27 18:24:42+08:00

--- LazyVim/lua/plugins/colorscheme.lua
+++ LazyVim/lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
     {
-        "Shatur/neovim-ayu",
-        -- "sainnhe/gruvbox-material",
+        -- "Shatur/neovim-ayu",
+        "sainnhe/gruvbox-material",
         config = function()
-            require("ayu").colorscheme()
-            -- vim.g.gruvbox_material_backgroud = "soft"
-            -- vim.g.gruvbox_material_current_word = "underline"
-            -- vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-            -- vim.g.gruvbox_material_visual = "red background"
-            -- vim.cmd.colorscheme("gruvbox-material")
-            -- -- Colors are applied automatically based on user-defined highlight groups.
-            -- -- There is no default value.
-            -- vim.cmd.highlight("IndentLine guifg=#808080")
-            -- -- Current indent line highlight
-            -- vim.cmd.highlight("IndentLineCurrent guifg=#FF6347")
+            -- require("ayu").colorscheme()
+            vim.g.gruvbox_material_backgroud = "soft"
+            vim.g.gruvbox_material_current_word = "underline"
+            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+            vim.g.gruvbox_material_visual = "red background"
+            vim.cmd.colorscheme("gruvbox-material")
+            -- Colors are applied automatically based on user-defined highlight groups.
+            -- There is no default value.
+            vim.cmd.highlight("IndentLine guifg=#808080")
+            -- Current indent line highlight
+            vim.cmd.highlight("IndentLineCurrent guifg=#FF6347")
         end,
     },
 }
