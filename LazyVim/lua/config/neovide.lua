7c108f734a911c711516b9e9025381d2105b8f91
zooeywm <zooeywm@gmail.com>
2025-02-11 10:08:13 +0800

    2025-02-11 10:08:13+08:00

--- LazyVim/lua/config/neovide.lua
+++ LazyVim/lua/config/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
     vim.g.neovide_input_ime = true
-    vim.g.neovide_scale_factor = 1.8
-    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_scale_factor = 1.0
+    vim.g.neovide_cursor_animation_length = 0.1
     vim.g.terminal_color_4 = "#268bd2"
 end
