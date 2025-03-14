052173a752f852e61bdce41ab7bfe6b573b53883
zooeywm <zooeywm@gmail.com>
2025-03-14 13:57:46 +0800

    2025-03-14 13:57:46+08:00

--- LazyVim/lua/plugins/colorscheme.lua
+++ LazyVim/lua/plugins/colorscheme.lua
@@ -11,17 +11,6 @@ return {
             vim.g.gruvbox_material_diagnostic_text_highlight = 1
             vim.g.gruvbox_material_inlay_hints_background = "none" --[[ or dimmed ]]
             vim.cmd.colorscheme("gruvbox-material")
-            -- Colors are applied automatically based on user-defined highlight groups.
-            -- There is no default value.
-            vim.cmd.highlight("IndentLine guifg=#808080")
-            -- Current indent line highlight
-            vim.cmd.highlight("IndentLineCurrent guifg=#FF6347")
-            local config = vim.fn["gruvbox_material#get_configuration"]()
-            local palette = vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
-            local set_hl = vim.fn["gruvbox_material#highlight"]
-
-            set_hl("Search", palette.none, palette.bg_visual_yellow)
-            set_hl("IncSearch", palette.none, palette.bg_visual_red)
         end,
     },
     {
