e61fa56d97707d28941749fff86637e04a78381f
zooeywm <zooeywm@gmail.com>
2025-09-18 18:51:49 +0800

    2025-09-18 18:51:49+08:00

--- LazyVim/lua/plugins/disabled.lua
+++ LazyVim/lua/plugins/disabled.lua
@@ -1,10 +1,10 @@
 return {
     { "folke/tokyonight.nvim", enabled = false },
-    { "williamboman/mason-lspconfig.nvim", enabled = false },
-    { "williamboman/mason.nvim", enabled = false },
+    { "mason-org/mason-lspconfig.nvim", enabled = false },
+    { "mason-org/mason.nvim", enabled = false },
     { "mfussenegger/nvim-lint", enabled = true },
     { "stevearc/conform.nvim", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "lukas-reineke/indent-blankline.nvim", enabled = false },
-    { "echasnovski/mini.indentscope", enabled = false },
+    { "nvim-mini/mini.indentscope", enabled = false },
 }
